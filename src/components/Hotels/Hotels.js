28e5808860403b4a5ecb286d29751c8f52ae2d8d
pawel <pawelzeg@gmail.com>
2021-02-24 00:19:05 +0100

    Added basic components, removed react logo

---
+++ src/components/Hotels/Hotels.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Hotel from './Hotel/Hotel'
+
+class Hotels extends Component {
+    render() {
+        return (
+            <div>
+                hotels
+                <Hotel />
+                <Hotel />
+                <Hotel />
+            </div>
+        );
+    }
+}
+
+
+export default Hotels;
